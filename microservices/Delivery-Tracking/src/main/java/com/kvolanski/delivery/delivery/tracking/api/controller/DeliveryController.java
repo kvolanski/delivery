3f6bffa15a96f4eebfa5ff35dc8dce1601d4aa41
Kevin  Volanski <kvolanski@sdnadigital.com>
2025-07-24 18:29:12 -0300

    Implantando consulta de Delivery

--- microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/api/controller/DeliveryController.java
+++ microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/api/controller/DeliveryController.java
@@ -2,11 +2,16 @@ package com.kvolanski.delivery.delivery.tracking.api.controller;
 
 import com.kvolanski.delivery.delivery.tracking.api.model.DeliveryInput;
 import com.kvolanski.delivery.delivery.tracking.domain.model.Delivery;
+import com.kvolanski.delivery.delivery.tracking.domain.repository.DeliveryRepository;
 import com.kvolanski.delivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -16,6 +21,7 @@ import java.util.UUID;
 public class DeliveryController {
 
     private final DeliveryPreparationService deliveryPreparationService;
+    private final DeliveryRepository deliveryRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -27,5 +33,18 @@ public class DeliveryController {
     public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput deliveryInput){
         return deliveryPreparationService.edit(deliveryId, deliveryInput);
     }
+
+    @GetMapping
+    public PagedModel<Delivery> findAll(@PageableDefault Pageable pageable){
+        return new PagedModel<>(
+                deliveryRepository.findAll(pageable)
+        );
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById(@PathVariable UUID deliveryId){
+        return deliveryRepository.findById(deliveryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 
 }
