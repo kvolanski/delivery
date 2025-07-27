80b3b1ace5ad4c761ca56c171cfd90587917022a
Kevin  Volanski <kvolanski@sdnadigital.com>
2025-07-27 20:48:08 -0300

    Retry Pattern com Resilience4j

--- Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClient.java
+++ Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClient.java
@@ -1,5 +1,6 @@
 package com.kvolanski.delivery.delivery.tracking.infrastructure.http.client;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -9,5 +10,6 @@ import org.springframework.web.service.annotation.PostExchange;
 public interface CourierAPIClient {
 
     @PostExchange("/payout-calculation")
+    @Retry(name = "Retry_CourierAPIClient_payoutCalculation")
     CourierPayoutResultModel payoutCalculation(@RequestBody CourierPayoutCalculationInput input);
 }
