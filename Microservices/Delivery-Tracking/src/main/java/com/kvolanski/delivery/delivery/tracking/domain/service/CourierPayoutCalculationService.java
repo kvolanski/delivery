fd91e3fe0d80f9e9cab85741a136d2c1672e8dc2
Kevin  Volanski <kvolanski@sdnadigital.com>
2025-07-27 17:06:28 -0300

    Implantando Spring RestClient com Eureka

--- Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/domain/service/CourierPayoutCalculationService.java
+++ Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/domain/service/CourierPayoutCalculationService.java
@@ -1,7 +1,5 @@
 package com.kvolanski.delivery.delivery.tracking.domain.service;
 
-import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 
 public interface CourierPayoutCalculationService {
--- Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ Microservices/Delivery-Tracking/src/main/java/com/kvolanski/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,5 +1,6 @@
 package com.kvolanski.delivery.delivery.tracking.infrastructure.http.client;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,10 +9,16 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class CourierAPIClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancendRestClientBuilder() {
+        return RestClient.builder();
+    }
     
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder){
-        RestClient restClient = builder.baseUrl("http://localhost:8081").build();
+        RestClient restClient = builder.baseUrl("http://courier-management").build();
         RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return proxyFactory.createClient(CourierAPIClient.class);
